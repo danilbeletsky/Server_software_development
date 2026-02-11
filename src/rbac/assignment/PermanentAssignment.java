1d0d1fd021b1dbcd7acb71e1dd1e14e43f3f141a
danilbeletsky <158756341+danilbeletsky@users.noreply.github.com>
2026-02-11 20:57:45 +0700

    1.7

--- src/rbac/assignment/PermanentAssignment.java
+++ src/rbac/assignment/PermanentAssignment.java
@@ -1,4 +1,31 @@
 package rbac.assignment;
 
-public class PermanentAssignment {
+import rbac.role.Role;
+import rbac.user.User;
+
+public class PermanentAssignment extends AbstractRoleAssignment {
+
+    private boolean revoked = false;
+
+    public PermanentAssignment(User user, Role role, AssignmentMetadata metadata) {
+        super(user, role, metadata);
+    }
+
+    public void revoke() {
+        revoked = true;
+    }
+
+    public boolean isRevoked() {
+        return revoked;
+    }
+
+    @Override
+    public boolean isActive() {
+        return !revoked;
+    }
+
+    @Override
+    public String assignmentType() {
+        return "PERMANENT";
+    }
 }
