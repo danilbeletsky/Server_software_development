daf46de48f8a4aea9d5891566a4132bb23eae2af
danilbeletsky <158756341+danilbeletsky@users.noreply.github.com>
2026-02-11 20:27:23 +0700

    1.5

--- src/rbac/assignment/RoleAssignment.java
+++ src/rbac/assignment/RoleAssignment.java
@@ -1,4 +1,14 @@
 package rbac.assignment;
 
+import rbac.role.Role;
+import rbac.user.User;
+
 public interface RoleAssignment {
+
+    String assignmentId();
+    User user();
+    Role role();
+    AssignmentMetadata metadata();
+    boolean isActive();
+    String assignmentType();
 }
